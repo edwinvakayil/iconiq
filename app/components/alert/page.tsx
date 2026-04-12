2d512eec6f218d594acaa0026e6780f0ffe7b9a6
edwiee <edwinvakayil@gmail.com>
2026-04-12 23:45:05 +0530

    fix bug

--- app/components/alert/page.tsx
+++ app/components/alert/page.tsx
@@ -332,10 +332,10 @@ export default function AlertPage() {
   const [previewKey, setPreviewKey] = useState(0);
 
   return (
-    <div className="flex min-h-[calc(100vh-0px)] w-full min-w-0 overflow-x-hidden">
+    <div className="flex min-h-[calc(100vh-0px)] w-full min-w-0">
       <SidebarNav />
 
-      <main className="min-w-0 flex-1">
+      <main className="min-w-0 flex-1 overflow-x-hidden">
         <div className="mx-auto w-full max-w-screen-2xl px-4 py-10 sm:px-6 sm:py-12 lg:px-10">
           <motion.nav
             animate={{ opacity: 1, y: 0 }}
